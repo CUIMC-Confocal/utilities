51403d02f9499a0c766376722a6e59098e058387
Theresa Swayne <theresaswayne@users.noreply.github.com>
2024-11-04 02:13:29 -0500

    delete shading, update merge

--- merge_cytation_channels.py
+++ merge_cytation_channels.py
@@ -38,22 +38,28 @@ for fname in os.listdir(inputDir):
 	if fname.startswith("."): # avoid dotfiles that have the extension and filename filter
 		continue
 	if fname.endswith(image_extension):
-		fnames.append(os.path.join(inputDir, fname))
+		#fnames.append(os.path.join(inputDir, fname))
+		fnames.append(fname)
 
 if len(fnames) < 1: # no files
-	raise Exception("No image files found in %s" % inputdir)
+	raise Exception("No image files found in %s" % inputDir)
 
 fnames = sorted(fnames) # so correct images are matched up
+print "Found", str(len(fnames)),"usable files"
 
 # fill the channel arrays
 for fname in fnames:
 	if C1name in fname:
 		C1.append(os.path.join(inputDir, fname))
+		#print "Adding C1 image", fname
 	elif C2name in fname:
 		C2.append(os.path.join(inputDir, fname))
+		#print "Adding C2 image", fname
 	elif C3name in fname:
 		C3.append(os.path.join(inputDir, fname))
+		#print "Adding C2 image", fname
 
+print (str(len(C1)), str(len(C2)), str(len(C3)))
 if ((len(C1) != len(C3)) or (len(C1) != len(C2)) or (len(C2) != len(C3))):
 	raise Exception("Unequal number of channel images found")
 
@@ -75,19 +81,26 @@ for i in range(0, len(C1)):
 	imp3.show()
 	
 	C1file = os.path.basename(C1[i])
-	#print C1file
+	print "File basename is", C1file
 	C2file = os.path.basename(C2[i])
 	C3file = os.path.basename(C3[i])
 	
 	# use brackets to prevent spaces in filename from causing problems
 	impMerge =IJ.run("Merge Channels...", "c1=[" + C1file + "] c2=[" + C2file + "] c3=[" + C3file + "] create")
-
-	#stkMerge = RGBStackMerge.mergeStacks(stk1, stk2, stk3, False) # of course this makes RGB goddammit
-	#impMerge = ImagePlus("merge", stkMerge)
 	
 	location = C1file.split("_")[0] # part up to the first underscore
+	print "Location is", location
 	outputName = string.join((location,"_merge", image_extension), "")
-	IJ.saveAs(impMerge, "Tiff", os.path.join(outputDir, outputName));
+	print "Output name is", outputName
+	IJ.saveAs(impMerge, "Tiff", os.path.join(outputDir, outputName))
+
+	# clean up
+	# impMerge.flush()
+	#impMerge.close()
+	impMerge = None
+	imp1 = None
+	imp2 = None
+	imp3 = None
 
 IJ.log("Finished")
 
